983ed1b839bdca56a20b7c3445354d397388804b
Tia Rose <shortyshort789@aol.com>
2019-04-24 21:03:50 -0700

    completed challenge 7. Passed

--- code-challenges/301/sort/challenges-03.test.js
+++ code-challenges/301/sort/challenges-03.test.js
@@ -134,7 +134,14 @@ const people = [
 ];
 
 const sortPeople = (arr) => {
-  // Solution code here...
+  arr.sort((a, b) => {
+    if(a.lastName < b.lastName){
+      return -1;
+    }else if(a.lastName > b.lastName){
+      return 1;
+    }
+  });
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
