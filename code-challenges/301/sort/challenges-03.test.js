cd0bc3e22b6898232d705783988447a713e3560c
Tia Rose <shortyshort789@aol.com>
2019-04-24 18:52:50 -0700

    completed challenge 4. Passed

--- code-challenges/301/sort/challenges-03.test.js
+++ code-challenges/301/sort/challenges-03.test.js
@@ -51,13 +51,21 @@ For example, ['Alphabet', 'alphabet', 'carrot', 'Zebra'] is correctly sorted, an
 
 const alphabetizeBetter = (arr) => {
   arr.sort((a, b) => {
-    if(a < b){
-      return -1;
-    }else if (a > b){
+    if (a.toLowerCase() > b.toLowerCase()) {
       return 1;
+    } else if (a.toLowerCase() < b.toLowerCase()) {
+      return -1;
+    } else {
+      return 0;
     }
   });
+
   return arr;
+
+  // arr.sort((a, b) => {
+  //   a.localeCompare(b);
+  // });
+  // return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -93,7 +101,18 @@ For example, [1, 14, 0.2, -281, 54782] is only correctly sorted in that order.
 ------------------------------------------------------------------------------------------------ */
 
 const sortNumbersByLength = (arr) => {
-  // Solution code here...
+  arr.sort((a, b) => {
+    // if(a.length < b.length){
+    //   return -1;
+    // }else if(a.length > b.length){
+    //   return 1;
+    // }else if(a.length === b.length){
+    //   return 0;
+    // }
+
+    return b-a ;
+  });
+  return arr;
 };
 
 /*-----------------------------------------------------------------------------------------------
