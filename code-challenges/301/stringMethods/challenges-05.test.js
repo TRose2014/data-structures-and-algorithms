c607c294b8d83bc2ace535aec219043bafa4051c
Tia Rose <shortyshort789@aol.com>
2019-04-26 16:58:51 -0700

    completed challenge 1. Passed

--- code-challenges/301/stringMethods/challenges-05.test.js
+++ code-challenges/301/stringMethods/challenges-05.test.js
@@ -11,7 +11,9 @@ For example, if the input is 'Welcome', the output will be:
 
 const howMuchPencil = (str) => {
   let result = [];
-  // Solution code here...
+  for(let i = 0; i <= str.length; i++){
+    result.push(str.slice([i]));
+  }
   return result;
 };
 
@@ -22,7 +24,8 @@ For example, wordsToCharList('gregor') returns ['g','r','e','g','o','r'].
 ------------------------------------------------------------------------------------------------ */
 
 const wordsToCharList = (arr) => {
-  // Solution code here...
+  let words = arr.split();
+  return words;
 };
 
 
