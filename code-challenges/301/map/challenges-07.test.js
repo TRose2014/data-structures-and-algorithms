333c62d03a8acb3ccf62882a8e769ab36bf64750
Tia Rose <shortyshort789@aol.com>
2019-04-30 19:51:35 -0700

    completed challenge 3. Passed

--- code-challenges/301/map/challenges-07.test.js
+++ code-challenges/301/map/challenges-07.test.js
@@ -34,7 +34,11 @@ Write a function named mapTwoToThe that produces the same output as your forLoop
 ------------------------------------------------------------------------------------------------ */
 
 const mapTwoToThe = (arr) => {
-  // Solution code here...
+  let result = [];
+  arr.map((n, i) => {
+    result.push(Math.pow(2, arr[i]));
+  });
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
