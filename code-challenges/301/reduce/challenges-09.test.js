575844ae5fc349a657f2b949891473a97f2f808e
Tia Rose <shortyshort789@aol.com>
2019-05-03 05:37:20 -0700

    completed challenge 3. Passed

--- code-challenges/301/reduce/challenges-09.test.js
+++ code-challenges/301/reduce/challenges-09.test.js
@@ -78,7 +78,7 @@ Note: You must use reduce for this challenge. You may not use the built-in .reve
 ------------------------------------------------------------------------------------------------ */
 
 const reversedString = (arr) => {
-  // Solution code here...
+  return arr.split('').reduce((acc, curr) => curr + acc);
 };
 
 /* ------------------------------------------------------------------------------------------------
