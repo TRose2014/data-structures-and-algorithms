45fe9e9596717b19b517918f51adc4a69500d96a
Tia Rose <shortyshort789@aol.com>
2019-05-05 11:04:34 -0700

    completed challenge 6. Passed

--- code-challenges/301/reduce/challenges-09.test.js
+++ code-challenges/301/reduce/challenges-09.test.js
@@ -171,7 +171,12 @@ const isPrime = (value) => {
 };
 
 const countPrimeNumbers = (arr) => {
-  // Solution code here...
+  return arr.reduce((acc, curr) => {
+    if(isPrime(curr)){
+      acc++;
+    }
+    return acc;
+  }, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
