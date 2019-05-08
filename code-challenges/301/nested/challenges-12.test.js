e0c0d7a1885e71d864b694efd63344d56097742f
Tia Rose <shortyshort789@aol.com>
2019-05-08 12:03:38 -0700

    completed challenge 3

--- code-challenges/301/nested/challenges-12.test.js
+++ code-challenges/301/nested/challenges-12.test.js
@@ -55,7 +55,17 @@ const errands = [
 ];
 
 const howManyTreats = (arr) => {
-  // Solution code here...
+  let treats = 0;
+
+  arr.forEach(list => {
+    list.items.forEach(item => {
+      if (item.name === 'Treats') {
+        treats += item.quantity;
+      }
+    });
+  });
+
+  return treats;
 };
 
 /* ------------------------------------------------------------------------------------------------
