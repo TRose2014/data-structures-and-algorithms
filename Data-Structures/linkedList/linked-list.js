cc525b7590297edb6453f50b4273357732f01223
Tia Rose <shortyshort789@aol.com>
2019-05-26 12:03:08 -0700

    trying to get git properly working

--- Data-Structures/linkedList/linked-list.js
+++ Data-Structures/linkedList/linked-list.js
@@ -2,7 +2,7 @@
 class Node{
   constructor (value){
     this.value = value;
-    this.ref = null;
+    this.next = null;
   }
 }
 
@@ -10,40 +10,69 @@ class Node{
 class LinkedList {
   constructor(){
     this.head = null;
+    this.length = 0;
   }
   insert(value){
-    let test = new Node(value);
-    list.head = test;
+    let newNode = new Node(value);
+    this.head = newNode;
     
   }
-  // includes(index){
-  //   if(index > -1){
-  //     let current = this.head;
-  //     console.log(current);
-  //     let i = 0;
-  //     while((current.next !== null) && (i < index)){
-  //       current = current.next;
-  //       console.log(current);
-  //       i++;
-  //     }
-  //     return current !== null ? current.value : undefined;
-  //   }else{
-  //     return undefined;
-  //   }
-  // }
+  add(value){
+    if(this.head === null){
+      this.head = new Node(value);
+      return this;
+    }else{
+      let currentNode = this.head;
 
-  
+      while(currentNode.next){
+        currentNode = currentNode.next;
+      }
+      currentNode.next = new Node(value);
+      return this;
+    }
+    // this.length++;
+  }
+  includes(value){
+    if(this.head.value === value){
+      return true;
+    }else{
+      let currentNode = this.head;
+      while(currentNode.next !== null){
+        if(currentNode.value === value){
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+  print(){
+    console.log(this.head);
+  }
 }
 
 const list = new LinkedList();
 
-console.log(list.head);
-list.insert(7);
-console.log(list.head);
+// console.log(list.head);
+// list.insert(7);
+// list.insert(5);
+// console.log(list.head);
 list.insert(2);
-console.log(list.head);
-list.includes(3);
-console.log(list.head);
+// console.log(list.head);
+// list.includes(3);
+// console.log(list.head);
+// console.log(list.add(4));
+// console.log(list.add(7));
+// console.log(list.add(2));
+list.add(4);
+list.add(1);
+list.add(2);
+// list.includes(4);
+// console.log(list.includes(4));
+console.log(list);
+// console.log(list.includes(2));
+
+
+
 // const head = new Node(2);
 // console.log(head);
 
@@ -58,3 +87,6 @@ console.log(list.head);
 //   console.log(current.value);
 //   current = current.next;
 // }
+
+
+//https://www.youtube.com/watch?v=AwMx3BzdzrM
