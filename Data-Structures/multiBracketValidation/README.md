3e93fa4c7f90b52df1cd6ea50fdcb61067e673b1
Tia Rose <shortyshort789@aol.com>
2019-06-12 13:46:53 -0700

    added photo for whiteboard

---
+++ Data-Structures/multiBracketValidation/README.md
@@ -0,0 +1,15 @@
+# Challenge Summary
+Multi-bracket Validation
+
+## Challenge Description
+Function should take a string as its only argument, and should return a boolean representing whether or not the brackets in the string are balanced. There are 3 types of brackets:
+
+Round Brackets : ()
+Square Brackets : []
+Curly Brackets : {}
+
+## Approach & Efficiency
+O(n)
+
+## Solution
+![Whiteboard 13](./assets/CC_13.jpg)
--- Data-Structures/multiBracketValidation/__tests__/multi-bracket-validation-test.js
+++ Data-Structures/multiBracketValidation/__tests__/multi-bracket-validation-test.js
@@ -5,29 +5,31 @@ bracketValidation = require('../multi-bracket-validation');
 
 describe('Multi-Bracket Validation', () => {
   it('should return false if length of string is an odd number', () => {
+    expect(string.length % 2 === 0).toBe(false);
 
   })
 
   it('should return true if an opening bracket is immediately followed by a same closing bracket', () => {
-
+    expect('()').toBe(true);
 
   })
 
   
   it('should return false if an opening bracket is immediately followed by a different cloding bracket', () => {
-    
+    expect('(]').toBe(false);
   })
   
   it('should return true if 3 opening brackets is immediately followed by 3 same closing brackets', () => {
-    
+    expect('((()))').toBe(true);
   })
   
-  it('should return false if 3 opening brackets is immediately followed by 3 same closing brackets', () => {
-    
-  })
 
   it('should return false if 2 opening brackets is immediately followed by 1 same brack and 1 different bracket', () => {
-    
+    expect('((()])').toBe(false);
+  })
+
+  it('should return true if an opening brackets is immediately followed by a same closing brackets with text in the middle', () => {
+    expect('(string)').toBe(true);
   })
 
 })
