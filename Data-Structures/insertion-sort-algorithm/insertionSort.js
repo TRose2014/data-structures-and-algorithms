4b6c8da89b1488ed7f1628223a3fb19c4380cd1c
Tia Rose <shortyshort789@aol.com>
2019-07-01 15:52:19 -0700

    have code working, now for tests

--- Data-Structures/insertion-sort-algorithm/insertionSort.js
+++ Data-Structures/insertion-sort-algorithm/insertionSort.js
@@ -1,20 +1,20 @@
-// 'use strict';
+'use strict';
 
 
-// function insertionSort(arr){
+function insertionSort(arr){
 
-//   if(arr.some(isNaN)){
-//     throw new Error('Not all elements are numbers');
-//   }
-//   for(let i = 0; i < arr.length; i++){
-//     let j = i - 1;
-//     let temp =  arr[i];
-//     while(j >= 0 && temp < arr[j]){
-//       arr[j + 1] = arr [j];
-//       j = j - 1;
-//     }
-//     arr[ j+ 1] = temp;
-//   }
-// }
+  if(arr.some(isNaN)){
+    throw new Error('Not all elements are numbers');
+  }
+  for(let i = 0; i < arr.length; i++){
+    let j = i - 1;
+    let temp =  arr[i];
+    while(j >= 0 && temp < arr[j]){
+      arr[j + 1] = arr [j];
+      j = j - 1;
+    }
+    arr[ j+ 1] = temp;
+  }
+}
 
-// module.exports = insertionSort;
+module.exports = insertionSort;
