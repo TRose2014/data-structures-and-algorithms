b663a0dda9952aafb4850962a35248dd84c8fbb6
Tia Rose <shortyshort789@aol.com>
2019-07-02 14:15:46 -0700

    working on code

--- Data-Structures/merge-sort-algorithm/mergeSort.js
+++ Data-Structures/merge-sort-algorithm/mergeSort.js
@@ -1,74 +1,74 @@
-'use strict';
+// 'use strict';
 
-function mergeSort(arr){
-  let n = arr.length;
+// function mergeSort(arr){
+//   let n = arr.length;
 
-  if(arr.length > 1){
-    let mid = n/2;
-    let b = arr[0];
-    let c = arr[mid + 1];
-  }
-}
+//   if(arr.length > 1){
+//     let mid = n/2;
+//     let b = arr[0];
+//     let c = arr[mid + 1];
+//   }
+// }
 
-// ALGORITHM Mergesort(arr)
-//     DECLARE n <-- arr.length
+// // ALGORITHM Mergesort(arr)
+// //     DECLARE n <-- arr.length
            
-//     if arr.length > 1
-//       DECLARE mid <-- n/2
-//       DECLARE b <-- arr[0...mid]
-//       DECLARE c <-- arr[mid...n]
-//       // break down the left side
-//       Mergesort(b)
-//       // break down the right side
-//       Mergesort(c)
-//       // merge the left and the right side together
-//       Merge(b, c, arr)
+// //     if arr.length > 1
+// //       DECLARE mid <-- n/2
+// //       DECLARE b <-- arr[0...mid]
+// //       DECLARE c <-- arr[mid...n]
+// //       // break down the left side
+// //       Mergesort(b)
+// //       // break down the right side
+// //       Mergesort(c)
+// //       // merge the left and the right side together
+// //       Merge(b, c, arr)
 
 
-function merge(b, c, a){
+// function merge(b, c, a){
 
-  let i = 0;
-  let j = 0;
-  let k = 0;
+//   let i = 0;
+//   let j = 0;
+//   let k = 0;
 
-  while(i < b && j < c){
-    if(b[i] <= c[j]){
-      a[k] = b[i];
-      i = i + 1;
-    }else{
-      a[k] = c[j];
-      j = j + 1;
-    }
+//   while(i < b && j < c){
+//     if(b[i] <= c[j]){
+//       a[k] = b[i];
+//       i = i + 1;
+//     }else{
+//       a[k] = c[j];
+//       j = j + 1;
+//     }
 
-    k = k + 1;
-  }
+//     k = k + 1;
+//   }
 
-  if(i === b.length){
-    c.push(a);
-  }else{
-    b.push(a);
-  }
-  return a;
-}
+//   if(i === b.length){
+//     c.push(a);
+//   }else{
+//     b.push(a);
+//   }
+//   return a;
+// }
 
-// ALGORITHM Merge(b, c, a)
-//     DECLARE i <-- 0
-//     DECLARE j <-- 0
-//     DECLARE k <-- 0
+// // ALGORITHM Merge(b, c, a)
+// //     DECLARE i <-- 0
+// //     DECLARE j <-- 0
+// //     DECLARE k <-- 0
 
-//     while i < b && j < c
-//         if b[i] <= c[j]
-//             a[k] <-- b[i]
-//             i <-- i + 1
-//         else
-//             a[k] = c[j]
-//             j <-- j + 1
+// //     while i < b && j < c
+// //         if b[i] <= c[j]
+// //             a[k] <-- b[i]
+// //             i <-- i + 1
+// //         else
+// //             a[k] = c[j]
+// //             j <-- j + 1
             
-//         k <-- k + 1
+// //         k <-- k + 1
 
-//     if i = b.length
-//        add remaining items in array c to array a
-//     else
-//        add remaining items in array b to array a
+// //     if i = b.length
+// //        add remaining items in array c to array a
+// //     else
+// //        add remaining items in array b to array a
        
-//     return a
+// //     return a
