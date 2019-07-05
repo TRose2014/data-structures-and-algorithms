040811f4eec733ccc4453594bf31cd44318c3a5a
Tia Rose <shortyshort789@aol.com>
2019-07-04 18:35:20 -0700

    have tests scaffolded, have outline for methods laid out with comments so I know which method is doing what

---
+++ Data-Structures/hashtable/__tests__/hashtable-test.js
@@ -0,0 +1,61 @@
+'use strict';
+
+// const hashtable =  require('../hashtable');
+
+beforeEach(() => {
+  // hastable
+});
+
+describe('hashtable table', () => {
+  it('should create an empy instance of a hashtable', () => {
+
+  });
+
+  it('should successfully hash a key to an in-range value', () => {
+
+  });
+});
+
+describe('add method', () => {
+  it('should add a key/value to the hashtable', () => {
+
+  });
+});
+
+describe('get method', () => {
+  it('should return the correct value based on a key', () => {
+
+  });
+});
+
+
+describe('get method', () => {
+  it('should return null for keys that do not exist in the hashtable', () => {
+
+  });
+});
+
+
+describe('collisions', () => {
+  it('should succesfully handle any collisions within the hashtable', () => {
+
+  });
+
+  it('should successfully retrieve a value from a bucket that has a collision', () => {
+
+  });
+});
+
+
+describe('contains method', () => {
+  it('should successfully return true/false if a key is or inside the hashtable or not', () => {
+
+  });
+});
+
+describe('hash method', () => {
+  it('should returns an index in the collection', () => {
+
+  });
+});
+
---
+++ Data-Structures/hashtable/hashtable.js
@@ -0,0 +1,36 @@
+'use strict';
+
+//hashtable
+
+/**
+ * @function add
+ * @param - key/value pair
+ * @description -  hashs the key, and add the key and value pair to the table
+ */
+
+
+/**
+ * @function get
+ * @param - key
+ * @description - returns the value from the table
+ */
+
+
+
+ 
+/**
+ * @function contains
+ * @param - key
+ * @description - returns a boolean, indicating if the key exists in the table already
+ * 
+ */
+
+
+
+
+
+/**
+  * @function hash
+  * @param - arbitrary key 
+  * @description - returns an index in the collection.
+  */
